c05aa403be836cf24b7bf470b9658c989e927db0
huowolf <274956285@qq.com>
2017-03-10 16:15:25 +0800

    sql文件

---
+++ ssm/user.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user` VALUES (1, 'zhangsan', 1, 30, 'test');
+INSERT INTO `user` VALUES (2, 'lisi', 1, 25, 'test');
+INSERT INTO `user` VALUES (3, 'wangwu', 1, 20, 'test');
+INSERT INTO `user` VALUES (4, 'test', 0, 26, 'test');
+INSERT INTO `user` VALUES (5, 'abc', 1, 35, 'test');
+INSERT INTO `user` VALUES (7, 'haha1111111111', 0, 30, 'test');
+INSERT INTO `user` VALUES (12, '李四', 1, 18, 'test');
